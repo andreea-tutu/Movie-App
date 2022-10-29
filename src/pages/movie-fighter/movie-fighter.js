58fe06a7e538f09f8b719f3a635d697baa99b813
Andreea Tutu <sol_andu@yahoo.com>
2022-10-29 19:13:18 +0300

    29.10

--- src/pages/movie-fighter/movie-fighter.js
+++ src/pages/movie-fighter/movie-fighter.js
@@ -1,8 +1,30 @@
 import "./movie-fighter.scss";
-import {Container, Row, Image} from "react-bootstrap";
+import { Container, Row, Image} from "react-bootstrap";
+// import { Form } from "react-bootstrap";
+// import { useState, useEffect } from "react";
+// import Autocomplete from "@mui/material/Autocomplete";
+// import TextField from "@mui/material/TextField";
+// import MovieList from "../../components/MovieList";
 import capture from "./Capture.PNG"
 
-function MovieFighter() {
+function MovieFighter(props) {
+  // const [searchValue, setSearchValue] = useState("");
+  // const [movie, setMovie] = useState("");
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.themoviedb.org/3/search/movie?api_key=e666a2cfa890d8a2bb19db4ca078ae7f&query=${searchValue}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       if (res.results) {
+  //         console.log(searchValue);
+  //         setMovie(res.results);
+  //       }
+  //     });
+  // }, [searchValue]);
+
+
   return (
     <Container fluid className="movie-fighter-page">
       <Row className="movie-row">
@@ -11,7 +33,20 @@ function MovieFighter() {
       <Row className="movie-row">
       <Image src={capture} alt="Movie Fight"></Image>
       </Row>
-      
+
+      {/* <Form.Control
+        type="text"
+        placeholder="Type to search..."
+        className="mb-sm-3 searchField"
+        value={props.value}
+        onChange={(event) => setSearchValue(event.target.value)}
+      /> */}
+
+      <div className="row">
+        {/* <MovieList
+          movies={movies}
+        /> */}
+      </div>
     </Container>
   );
 }
