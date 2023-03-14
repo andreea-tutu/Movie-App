5d2ad5303f5e6df12c931f228d38f6f21a84d5fe
Andreea Tutu <sol_andu@yahoo.com>
2023-03-14 22:03:27 +0200

    Fix Hash Router

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ import { HashRouter } from "react-router-dom";
 //   </React.StrictMode>
 // );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App/>
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
